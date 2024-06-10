6e8de0f4aa4d925fe05bfc5b8f8dbfcba07ab1fb
ngimc1 <115123441+ngimc1@users.noreply.github.com>
2024-06-10 13:32:07 -0400

    corrected file path

--- app/profile/page.jsx
+++ app/profile/page.jsx
@@ -35,7 +35,7 @@ const MyProfile = () => {
 
         if(hasConfirmed) {
             try {
-                await fetch(`/api/prompt/${post._id.toString()}`, {
+                await fetch(`/api/quote/${post._id.toString()}`, {
                     method: 'DELETE'
                 });
 
